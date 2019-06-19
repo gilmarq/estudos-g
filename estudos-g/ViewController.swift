a02c9a7dc406e12fcae2d3829a823ab0cb85ae43
gilmarqueiroz <gilmardequeiroz@hotmail.com>
2019-06-18 22:54:33 -0300

    Initial Commit

---
+++ estudos-g/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  estudos-g
+//
+//  Created by Gilmar Queiroz on 18/06/19.
+//  Copyright © 2019 Gilmar Queiroz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ estudos-gTests/estudos_gTests.swift
@@ -0,0 +1,34 @@
+//
+//  estudos_gTests.swift
+//  estudos-gTests
+//
+//  Created by Gilmar Queiroz on 18/06/19.
+//  Copyright © 2019 Gilmar Queiroz. All rights reserved.
+//
+
+import XCTest
+@testable import estudos_g
+
+class estudos_gTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
